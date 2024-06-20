0f188030ab938ca961ff624029939999e88af21a
ErnestoDomenech <ernestodomenech@live.com>
2024-06-20 19:12:40 +0200

    clase 20240620

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ dotenv.config({path:'./config/config.env'});
 //crear una app a partir de la clase express
 const app = express();
 
+//especificar el archivo de rutas campus.js - alumnos.js
+const campus = require('./routes/campus');
+const alumnos = require('./routes/alumnos');
+
+//montar (activar) rutas
+app.use('/api/v1/campus', campus);
+app.use('/api/v1/alumnos', alumnos);
 //establecer el puerto en localhost
 const PORT = process.env.PORT || 5000;
 
