869dfcac5651c4fde13b99d15fde7aa58ac2d5d5
ErnestoDomenech <ernestodomenech@live.com>
2024-07-05 17:47:26 +0200

    bug Mongo_uri

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const connmdb =  'mongodb+srv://devadmin:aKDrbJLz6Ab5c4C@devcamp.mclrko7.mongodb.net/?retryWrites=true&w=majority&appName=devcamp';
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(connectDB);
 
     console.log('MongoDB connected');
 }
